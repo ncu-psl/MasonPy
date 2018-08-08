ba5ecb897a4769c4a0f4fde8237dc8d01f65d0db
SeanLiuNCU <32979665+SeanLiuNCU@users.noreply.github.com>
2018-08-08 17:24:00 +0800

    Modified
    Modified

--- VPL/CompileList.py
+++ VPL/CompileList.py
@@ -1,19 +1,28 @@
-from ExtremePoint import*
-from Mode import*
-from Decision import*
+ModuleandClass = [['ExtremePoint', 'ExtremePointMode'], ['Mode', 'originalMode'], ['Decision', 'Decide']]  # [file.py, class]
 
-def checkModule(string):
-    AllModule = [ExtremePoint, Mode, Decision]
+
+for i in ModuleandClass:
+    exec('from '+ i[0] + ' import*')
+
+global AllModule
+AllModule = [ i[1] for i in ModuleandClass ]
+
+
+
+def buildObj(string):
+    global AllModule
     
-
-def execProcess(string):
-    buildobject = eval("Formula."+string+"()")
-    return buildobject
-    
-def evalDecision(string):
-    buildobject = eval(string+"()")
-    return buildobject
-
+    if string in AllModule:
+        buildobject = eval(string+'()')
+#        return  obj
+    else:
+        print('Not found!')
+        buildobject = False   
+#==============================================================================
+#         msg = 'Not found!'
+#         return msg
+#==============================================================================
+    return  buildobject
 
 
 
@@ -36,24 +45,24 @@ def FindNextBlock(list, Connectionline):
 
 
 def execBlockChart(list):
-    Process  = 0
-    Decision = 0
-    Loop     = 0
+    number_Process  = 0
+    number_Decision = 0
+    number_Loop     = 0
     
     for i in range(len(list)):
         string = list[i][1]
         if string.find("Mode")!= -1:
-            Process += 1
+            number_Process += 1
         if string.find("Check")!= -1:
-            Decision += 1 
+            number_Decision += 1 
         if string.find("Loop")!= -1:
-            Loop += 1
-    print("Process", Process)    
-    print("Decision", Decision)
-    print("Loop", Loop)
+            number_Loop += 1
+    print("Process", number_Process)    
+    print("Decision", number_Decision)
+    print("Loop", number_Loop)
  
+    
     flag = -1
-    lastBlock =""
     if list[0][1] == "Start":
         Connectionline = list[0][3][0]
         flag = FindNextBlock(list, Connectionline)
@@ -67,12 +76,6 @@ def execBlockChart(list):
         string = list[flag][1]
    
         if string.find("Mode") != -1:
-            if (Parameter.CurrentTime == (len(Parameter.TimeSeries)-1)):
-                break
-         
-            Parameter.ModeStack.append(string)
-            Parameter.CurrentTime += 1
-            
             if lastBlock != "Loop":
                resetLoopCounter(list)
                
@@ -84,7 +87,7 @@ def execBlockChart(list):
 
             
                 
-        if string.find("Check") != -1:
+        elif string.find("Check") != -1:
             if evalDecision(string):
                 Connectionline = list[flag][3][0]
                 flag = FindNextBlock(list, Connectionline)
@@ -96,7 +99,7 @@ def execBlockChart(list):
                 
              
                 
-        if string.find("Loop") != -1:
+        elif string.find("Loop") != -1:
             if list[flag][4] == list[flag][5]:
                 list[flag][4] = 0
                 Connectionline = list[flag][3][0]
@@ -108,5 +111,35 @@ def execBlockChart(list):
                 flag = FindNextBlock(list, Connectionline)
                 lastBlock = string
 
-        if flag == -1:   
-            break
+        else:
+            if flag == -1:   
+                break
+            
+            
+            
+if __name__=='__main__':
+    
+#==============================================================================
+#     list=[
+#         ['Start0', 'Start', [], ['line_0']],
+# ['Mode_ThreePhaseShortCircuit0', 'Mode_ThreePhaseShortCircuit', ['line_0', 'line_16', 'line_17', 'line_19', 'line_24'], ['line_1']],
+# ['Check_MaxMagBrake0', 'Check_MaxMagBrake', ['line_1'], ['line_2', 'line_15']],
+# ['Check_MaxWindSpeed_ThreePhaseShortCircuit0', 'Check_MaxWindSpeed_ThreePhaseShortCircuit', ['line_15'], ['line_3', 'line_16']],
+# ['Loop0', 'Loop', ['line_3'], ['line_4', 'line_17'], 0, 2000],
+# ['Mode_MaxPower0', 'Mode_MaxPower', ['line_4', 'line_18'], ['line_5']],
+# ['Check_CutOut0', 'Check_CutOut', ['line_5'], ['line_6', 'line_18']],
+# ['Mode_MaxTorqueCurrent0', 'Mode_MaxTorqueCurrent', ['line_6', 'line_20'], ['line_7']],
+# ['Check_MaxMagBrake1', 'Check_MaxMagBrake', ['line_7'], ['line_8', 'line_19']],
+# ['Check_RPM_Increase0', 'Check_RPM_Increase', ['line_8'], ['line_9', 'line_20']],
+# ['Mode_MaxTorqueCurrent1', 'Mode_MaxTorqueCurrent', ['line_9', 'line_21'], ['line_10']],
+# ['Loop1', 'Loop', ['line_10'], ['line_11', 'line_21'], 0, 8],
+# ['Mode_MaxTorqueCurrent_MagBrake0', 'Mode_MaxTorqueCurrent_MagBrake', ['line_2', 'line_11', 'line_14'], ['line_12']],
+# ['Check_MaxMagBrake2', 'Check_MaxMagBrake', ['line_12'], ['line_14', 'line_22']],
+# ['Mode_ThreePhaseShortCircuit_MagBrake0', 'Mode_ThreePhaseShortCircuit_MagBrake', ['line_22', 'line_23'], ['line_13']],
+# ['Loop2', 'Loop', ['line_13'], ['line_24', 'line_23'], 0, 200]                         #15  
+#     ]
+#==============================================================================
+    
+    testList = [['Start', '']]            
+            
+            
