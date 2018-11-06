9fa43f2bb45dfe25331ee862908d3d73f5fb62c6
SeanLiuNCU <carbon9500@gmail.com>
2018-11-06 16:04:36 +0800

    修改搜尋檔案路徑
    修改搜尋檔案路徑

--- VPL/SetModule.py
+++ VPL/SetModule.py
@@ -1,14 +1,46 @@
 import os
 
+#==============================================================================
+# def getFile():
+#     AllFile = []
+#     filesFolder = [f for f in os.listdir('.') if os.path.isdir(f)]
+#     for f in filesFolder:
+#         filesPY = [i for i in os.listdir('./'+ str(f)) if not os.path.isdir(i) and os.path.splitext(i)[1] == '.py']
+#         for i in filesPY:
+#             file = f+'.'+os.path.splitext(i)[0]
+#             AllFile.append(file)
+#     return AllFile   
+#==============================================================================
+
 def getFile():
     AllFile = []
     filesFolder = [f for f in os.listdir('.') if os.path.isdir(f)]
+    
     for f in filesFolder:
-        filesPY = [i for i in os.listdir('./'+ str(f)) if not os.path.isdir(i) and os.path.splitext(i)[1] == '.py']
-        for i in filesPY:
-            file = f+'.'+os.path.splitext(i)[0]
-            AllFile.append(file)
-    return AllFile     
+        AllFile.extend(findFile(f))
+    for i in range(len(AllFile)):
+        AllFile[i] = AllFile[i].replace('/', '.') 
+    return AllFile 
+
+def findFile(folder = None):
+    if folder == None:
+        return ;
+    else:
+        filesFolder = []
+        if type(folder) is str:
+            folder_path = '/'+ folder
+        for f in os.listdir('.'+ folder_path):
+            if os.path.isdir(f):
+                pass
+            else:
+                if os.path.splitext(f)[1] == '.py':
+                    file = folder+'.'+os.path.splitext(f)[0]
+                    filesFolder.append(file) 
+                if os.path.splitext(f)[1] == '':
+                    file = folder+'/'+os.path.splitext(f)[0]
+                    filesFolder.extend(findFile(file))               
+        return filesFolder    
+  
 
 
 def getClass():
@@ -27,9 +59,15 @@ def getClass():
 if __name__=='__main__':
     import unittest
     
-    class test(unittest.TestCase):     
+    class test_GetClass(unittest.TestCase):     
         def test_isStrings(self):
-            ModuleandClass = getModuleandClass()
-            for i in ModuleandClass:
+            AllClass = getClass()
+            for i in AllClass:
                 self.assertIs(type(i), str)
+    class test_GetFile(unittest.TestCase):    
+        def test_isStrings(self):
+            file = getFile()
+            for i in file:
+                self.assertIs(type(i), str)        
+                
     unittest.main()
