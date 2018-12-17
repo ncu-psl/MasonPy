9e8691d62d6bfdcebdd4b366737cbeb3b7d05b47
SeanLiuNCU <carbon9500@gmail.com>
2018-12-17 23:57:48 +0800

    unittest Missing Lines
    debug MissingLineException

--- VPL/FrameworkDebugger.py
+++ VPL/FrameworkDebugger.py
@@ -1,12 +1,27 @@
 import sys
 
+#==============================================================================
+# class HasErrorException(Exception):
+#     def __init__(self, ErrorMsg = [0], err=''):
+#         self.err = err
+#         Exception.__init__(self, self.err)
+#         
+# def MissingLineRaise():
+#     raise HasErrorException()   
+#==============================================================================
+
 class MissingLineException(Exception):
-    def __init__(self, InputNum = 0, OutputNum = 0, err='Error: \"連接線錯誤\" \n'):
+    def __init__(self, InputNum = 0, OutputNum = 0, err='Error: \"連接線錯誤\"\n'):
         InputErrMsg = self.getErrMsg(self.checkErrFlag(InputNum), self.setInputErrMsg())
         OutputErrMsg = self.getErrMsg(self.checkErrFlag(OutputNum), self.setOutputErrMsg())
         self.err = err + InputErrMsg + OutputErrMsg
+        if (InputErrMsg + OutputErrMsg) =='':
+            self.err = ''
         Exception.__init__(self, self.err)
 
+    def getFinalErrMsg(self):
+        return self.err
+    
     def checkErrFlag(self, LinesNum):
         if LinesNum < 1:
             ErrFlag = 0
@@ -25,7 +40,9 @@ class MissingLineException(Exception):
         return ErrMsg   
     
     def getErrMsg(self, ErrFlag, ErrMsgDict):
-        err =  ErrMsgDict[str(ErrFlag)] +'\n'
+        err =  ErrMsgDict[str(ErrFlag)]
+        if err != '':
+            err =  err +'\n'
         return err
 
 
@@ -88,14 +105,14 @@ class DecisionMissingLineException(MissingLineException):
         return ErrMsg
     
     def setOutputErrMsg(self):
-        ErrMsg = {'0': 'DecisionDecision 輸出線必須為兩條,目前不足2條', '1': 'Decision 輸出線必須為兩條,目前不足1條', '2': '', '3': 'Decision 輸出線僅能兩條,目前過多'}
+        ErrMsg = {'0': 'Decision 輸出線必須為兩條,目前不足2條', '1': 'Decision 輸出線必須為兩條,目前不足1條', '2': '', '3': 'Decision 輸出線僅能兩條,目前過多'}
         return ErrMsg
 
 
 #==============================================================================
 # Loop Lines
 #==============================================================================
-class LoopMissingLineException(MissingLineException):
+class LoopMissingLineException(DecisionMissingLineException):
     def setInputErrMsg(self):
         ErrMsg = {'0': 'Loop 缺少輸入線', '1': '', '2': '',  '3': ''}
         return ErrMsg
@@ -112,7 +129,8 @@ def MissingLineRaise(BlockName, InputNum, OutputNum):
     BlockDict = {'Start':'StartMissingLineException', 'End':'EndMissingLineException', 
     'Process':'ProcessMissingLineException', 'Decision':'DecisionMissingLineException', 'Loop':'LoopMissingLineException'}    
     print('raise '+ BlockDict[BlockName] + '('+str(InputNum) +','+ str(OutputNum) +')')
-    exec('raise '+ BlockDict[BlockName] + '('+str(InputNum) +','+ str(OutputNum) +')')
+    if eval(BlockDict[BlockName] + '('+str(InputNum) +','+ str(OutputNum) +')'+'.getFinalErrMsg()') != '':
+           exec('raise '+ BlockDict[BlockName] + '('+str(InputNum) +','+ str(OutputNum) +')')
 
    
     
@@ -129,33 +147,130 @@ def MissingLineRaise(BlockName, InputNum, OutputNum):
   
 
 errmsg = ''
-while 1:
+
+def TestMissingLineRaise(BlockName, x, y):
+    MissingLinemsg = None
     try:
-        x = int(input('輸入x:'))
-        y = int(input('輸入y:'))
-        print(x, y)
-        MissingLineRaise('End', x, y)
-#==============================================================================
-#         if x is False or y is False :
-#             MissingLineRaise(x, y)
-#==============================================================================
-            
-        
+        MissingLineRaise(BlockName, x, y)
     except MissingLineException as e:
-        MissingLinemsg = str(sys.exc_info()[1])+'\n'
-        print(e) 
+        MissingLinemsg = str(sys.exc_info()[1])
+
     print('###################')    
     print(MissingLinemsg)
-    
-#==============================================================================
-# if __name__ =='__main__':
-# 
-#     import unittest    
-#     class test(unittest.TestCase):
-#         
-#         def test_1_currentTime(self):
-#             try:
-#                 StartMissingMissingLineRaise(x, y)       
-#             except MissingLineException as e:
-#==============================================================================
-                
+    return MissingLinemsg 
+
+
+if __name__ =='__main__':
+    import unittest    
+    class test(unittest.TestCase): 
+        # Start (輸入線,輸出線)=(0, 1)
+        # Start 輸入線正確 輸出線正確
+        def test_Start_1_MissingLineRaise(self):
+            self.assertIs(TestMissingLineRaise('Start', 0, 1), None)
+        # Start 輸入線正確 輸出線錯誤(缺少)
+        def test_Start_2_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Start', 0, 0), 'Error: "連接線錯誤"\nStart 缺少輸出線\n')
+        # Start 輸入線錯誤(不得有輸入線) 輸出線錯誤(缺少)
+        def test_Start_3_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Start', 2, 0), 'Error: "連接線錯誤"\nStart 不得有輸入線\nStart 缺少輸出線\n')
+        # Start 輸入線錯誤(不得有輸入線) 輸出線錯誤(過多)
+        def test_Start_4_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Start', 3, 3), 'Error: "連接線錯誤"\nStart 不得有輸入線\nStart 僅能一條輸出線\n')
+        # Start 輸入線正確 輸出線錯誤(缺少)
+        def test_Start_5_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Start', 0, 0), 'Error: "連接線錯誤"\nStart 缺少輸出線\n')
+        # Start 輸入線正確 輸出線錯誤(過多)
+        def test_Start_6_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Start', 0, 2), 'Error: "連接線錯誤"\nStart 僅能一條輸出線\n')
+            
+            
+        # End (輸入線,輸出線)=(n>0, 0)
+        # End 輸入線正確 輸出線正確
+        def test_End_1_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('End', 1, 0), None)
+        def test_End_2_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('End', 5, 0), None)    
+        
+        # End 輸入線正確 輸出線錯誤
+        def test_End_3_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('End', 1, 1), 'Error: "連接線錯誤"\nEnd 不得有輸出線\n')
+        def test_End_4_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('End', 5, 5), 'Error: "連接線錯誤"\nEnd 不得有輸出線\n')    
+        # End 輸入線錯誤(缺少) 輸出線正確
+        def test_End_5_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('End', 0, 0), 'Error: "連接線錯誤"\nEnd 缺少輸入線\n')
+        # End 輸入線錯誤(缺少) 輸出線錯誤
+        def test_End_6_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('End', 0, 1), 'Error: "連接線錯誤"\nEnd 缺少輸入線\nEnd 不得有輸出線\n')
+        def test_End_7_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('End', 0, 5), 'Error: "連接線錯誤"\nEnd 缺少輸入線\nEnd 不得有輸出線\n')   
+        
+        # Process (輸入線,輸出線)=(n>0, n>0)
+        def test_Process_1_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Process', 1, 4), None)
+        def test_Process_2_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Process', 5, 1), None)
+        def test_Process_3_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Process', 3, 3), None)
+        # End 輸入線正確 輸出線錯誤
+        def test_Process_4_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Process', 3, 0), 'Error: "連接線錯誤"\nProcess 缺少輸出線\n')
+        # End 輸入線錯誤 輸出線正確
+        def test_Process_5_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Process', 0, 3), 'Error: "連接線錯誤"\nProcess 缺少輸入線\n')
+        # End 輸入線錯誤 輸出線錯誤
+        def test_Process_6_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Process', 0, 0), 'Error: "連接線錯誤"\nProcess 缺少輸入線\nProcess 缺少輸出線\n')    
+        
+        # Decision (輸入線,輸出線)=(n>0, 2)
+        def test_Decision_1_MissingLineRaise(self):
+            self.assertIs(TestMissingLineRaise('Decision', 1, 2), None)
+        def test_Decision_2_MissingLineRaise(self):
+            self.assertIs(TestMissingLineRaise('Decision', 5, 2), None)
+        # Decision 輸入線正確 輸出線錯誤(缺少)
+        def test_Decision_3_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 1, 0), 'Error: "連接線錯誤"\nDecision 輸出線必須為兩條,目前不足2條\n')
+        def test_Decision_4_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 1, 1), 'Error: "連接線錯誤"\nDecision 輸出線必須為兩條,目前不足1條\n')
+        # Decision 輸入線正確 輸出線錯誤(過多)
+        def test_Decision_5_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 1, 3), 'Error: "連接線錯誤"\nDecision 輸出線僅能兩條,目前過多\n')
+        def test_Decision_6_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 1, 5), 'Error: "連接線錯誤"\nDecision 輸出線僅能兩條,目前過多\n')    
+        # Decision 輸入線錯誤 輸出線正確
+        def test_Decision_7_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 0, 2), 'Error: "連接線錯誤"\nDecision 缺少輸入線\n')
+        # Decision 輸入線錯誤 輸出線錯誤
+        def test_Decision_8_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 0, 0), 'Error: "連接線錯誤"\nDecision 缺少輸入線\nDecision 輸出線必須為兩條,目前不足2條\n')
+        def test_Decision_9_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 0, 1), 'Error: "連接線錯誤"\nDecision 缺少輸入線\nDecision 輸出線必須為兩條,目前不足1條\n')
+        def test_Decision_10_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Decision', 0, 10), 'Error: "連接線錯誤"\nDecision 缺少輸入線\nDecision 輸出線僅能兩條,目前過多\n')
+        
+        # Loop (輸入線,輸出線)=(n>0, 2)
+        def test_Loop_1_MissingLineRaise(self):
+            self.assertIs(TestMissingLineRaise('Loop', 1, 2), None)
+        def test_Loop_2_MissingLineRaise(self):
+            self.assertIs(TestMissingLineRaise('Loop', 5, 2), None)
+        # Loop 輸入線正確 輸出線錯誤(缺少)
+        def test_Loop_3_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 1, 0), 'Error: "連接線錯誤"\nLoop 輸出線必須為兩條,目前不足2條\n')
+        def test_Loop_4_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 1, 1), 'Error: "連接線錯誤"\nLoop 輸出線必須為兩條,目前不足1條\n')
+        # Loop 輸入線正確 輸出線錯誤(過多)
+        def test_Loop_5_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 1, 3), 'Error: "連接線錯誤"\nLoop 輸出線僅能兩條,目前過多\n')
+        def test_Loop_6_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 1, 5), 'Error: "連接線錯誤"\nLoop 輸出線僅能兩條,目前過多\n')    
+        # Loop 輸入線錯誤 輸出線正確
+        def test_Loop_7_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 0, 2), 'Error: "連接線錯誤"\nLoop 缺少輸入線\n')
+        # Loop 輸入線錯誤 輸出線錯誤
+        def test_Loop_8_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 0, 0), 'Error: "連接線錯誤"\nLoop 缺少輸入線\nLoop 輸出線必須為兩條,目前不足2條\n')
+        def test_Loop_9_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 0, 1), 'Error: "連接線錯誤"\nLoop 缺少輸入線\nLoop 輸出線必須為兩條,目前不足1條\n')
+        def test_Loop_10_MissingLineRaise(self):
+            self.assertEqual(TestMissingLineRaise('Loop', 0, 10), 'Error: "連接線錯誤"\nLoop 缺少輸入線\nLoop 輸出線僅能兩條,目前過多\n')
+    unittest.main()             
