395b02e3badbce4694f3f2dcc17d55c0bec09e9d
SeanLiuNCU <carbon9500@gmail.com>
2019-05-30 12:36:19 +0800

    刪出多餘檔案
    刪出多餘檔案

--- Wind Turbine Control System(project)/project(version1.4.1)/Block.py
+++
@@ -1,7 +0,0 @@
-class block(object):
-    def __init__(self, blockname, inputMode, inputlist, outlist, cycletime):
-        self.Mode      = blockname
-        self.inputMode = inputMode
-        self.input     = inputlist
-        self.outlist   = outlist
-        self.cycletime = cycletime
--- Wind Turbine Control System(project)/project(version1.4.1)/BlockMode.py
+++
@@ -1,7 +0,0 @@
-from Block import*
-class blockmode(block):
-    def __init__(self, Mode, inputlist, outlist, cycletime=0):
-        self.Mode      = Mode
-        self.input     = inputlist
-        self.outlist   = outlist[0]
-        self.cycletime = 0
